f087f778157335668d3f2585f362c65c08008b87
AlexAntonny <varalex@gmail.com>
2024-08-08 21:38:34 -0500

    java-basico-2-avargas1

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        int sumatoria = 0;
+        int s = 1;
+
+        while (s <= 10) {
+            sumatoria += s;
+            s++;
+            System.out.println("Sumatoria de los numeros es :" + sumatoria);
+        }
+
+    }
+}
